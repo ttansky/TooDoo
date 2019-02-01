0d3f2e4608b3b3a3ed9e604428ee2d789c91de49
Taras Tanskiy <tarastansky@gmail.com>
2019-02-01 16:08:37 +0200

    Initial Commit

---
+++ TooDoo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TooDoo
+//
+//  Created by Тарас on 2/1/19.
+//  Copyright © 2019 Taras. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
